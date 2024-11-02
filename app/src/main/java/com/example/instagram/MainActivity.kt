3a9726e65a4fd9625c59722b4ff463ff47acc8d8
Prince Ikede <princeikede@gmail.com>
2024-11-01 20:56:22 -0400

    Adds dependencies

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -3,44 +3,58 @@ package com.example.instagram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.instagram.auth.SignupScreen
 import com.example.instagram.ui.theme.InstagramTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // Don't need edge to edge for now
         setContent {
             InstagramTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    InstagramApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+sealed class DestinationScreen(val route: String) {
+    object Signup : DestinationScreen("signup")
+}
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InstagramApp(
+    modifier: Modifier = Modifier,
+    vm: IgViewModel = hiltViewModel(),
+    navController: NavHostController = rememberNavController(),
+) {
+    NavHost(navController = navController, startDestination = DestinationScreen.Signup.route) {
+        composable(DestinationScreen.Signup.route) {
+            SignupScreen(navController = navController, vm = vm)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     InstagramTheme {
-        Greeting("Android")
+        InstagramApp()
     }
 }
