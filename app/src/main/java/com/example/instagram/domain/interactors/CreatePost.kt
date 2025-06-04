9b978cf528d0f793575d24ce95378758fc9737c7
Prince Ikede <davidikede24@gmail.com>
2025-06-03 22:09:59 -0400

    new post changes

---
+++ app/src/main/java/com/example/instagram/domain/interactors/CreatePost.kt
@@ -0,0 +1,46 @@
+package com.example.instagram.domain.interactors
+
+import com.example.instagram.domain.core_domain.Interactor
+import com.example.instagram.domain.network.CoroutineDispatchers
+import com.example.instagram.models.PostData
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.util.UUID
+import javax.inject.Inject
+
+class CreatePost @Inject constructor(
+    private val getUser: GetUser,
+    private val db: FirebaseFirestore,
+    private val dispatchers: CoroutineDispatchers,
+) : Interactor<CreatePost.Params, Unit>() {
+
+    data class Params(val imageUri: String, val description: String?)
+
+    override suspend fun doWork(params: Params) {
+        withContext(dispatchers.io) {
+            val currentUser = getUser.execute()
+            val postUuid = UUID.randomUUID().toString()
+
+            val fillerWords = listOf("the", "be", "to", "is", "of", "and", "or", "a", "in", "it")
+            val searchTerms = params.description.orEmpty()
+                .split(" ", ".", ",", "?", "!", "#")
+                .map { it.lowercase() }
+                .filter { it.isNotEmpty() && it !in fillerWords }
+
+            val post = PostData(
+                postId = postUuid,
+                userId = currentUser.userId,
+                username = currentUser.userName,
+                userImage = currentUser.imageUrl,
+                postImage = params.imageUri.toString(),
+                postDescription = params.description,
+                time = System.currentTimeMillis(),
+                likes = listOf(),
+                searchTerms = searchTerms
+            )
+            db.collection("posts").document(postUuid).set(post)
+                .await()
+        }
+    }
+}
--- app/src/main/java/com/example/instagram/new_post/NewPostViewModel.kt
+++ app/src/main/java/com/example/instagram/new_post/NewPostViewModel.kt
@@ -2,80 +2,42 @@ package com.example.instagram.new_post
 
 import android.net.Uri
 import android.util.Log
-import androidx.compose.runtime.mutableStateOf
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.instagram.common.extensions.OneTimeEvent
 import com.example.instagram.common.extensions.ViewEventSinkFlow
-import com.example.instagram.common.util.Constants.POSTS
-import com.example.instagram.common.util.Constants.USERS
-import com.example.instagram.common.util.convertPosts
 import com.example.instagram.coroutineExtensions.combine
 import com.example.instagram.coroutineExtensions.stateInDefault
-import com.example.instagram.models.PostData
-import com.example.instagram.models.User
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
+import com.example.instagram.domain.interactors.CreatePost
+import com.example.instagram.domain.interactors.UploadImage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
-import java.util.UUID
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 internal class NewPostViewModel @Inject constructor(
-    val auth: FirebaseAuth,
-    val db: FirebaseFirestore,
-    val storage: FirebaseStorage,
+    private val createPost: CreatePost,
+    private val uploadImage: UploadImage,
 ) : ViewModel() {
     private val default = NewPostViewState.Companion.Empty
     private val inProgressState = MutableStateFlow(default.inProgress)
     private val isSignedInState = MutableStateFlow(default.isSignedIn)
     private val descriptionState = MutableStateFlow(default.description)
-    private val userState = MutableStateFlow(default.user)
     private val notificationState = MutableStateFlow(default.notification)
     private val errorState = MutableStateFlow(default.error)
-    val refreshPostsProgressState = MutableStateFlow(false)
-    val postsState = mutableStateOf<List<PostData>>(listOf())
 
     val state = combine(
         inProgressState,
         isSignedInState,
         descriptionState,
-        userState,
         notificationState,
         errorState,
         eventSink(),
         ::NewPostViewState
     ).stateInDefault(viewModelScope, default)
-
-    init {
-        val currentUser = auth.currentUser
-        currentUser?.uid?.let { userId ->
-            getUserData(userId) // todo cash getUserData after getting with interactor value
-        }
-    }
-
-    private fun getUserData(uid: String) {
-        inProgressState.value = true
-        db.collection(USERS).document(uid)
-            .get()  // TODO create a helper that gets the document from firebase in an Interactor
-            .addOnSuccessListener {
-                val user = it.toObject(User::class.java)
-                userState.value = user
-                inProgressState.value = false
-                isSignedInState.value = true
-                // todo create interactor for this refresh
-                refreshPosts() // TODO add to other getUserData functions
-            }
-            .addOnFailureListener {
-                errorState.value = it
-                isSignedInState.value = false
-                Log.e("*** Failed to createOrUpdateProfile", it.localizedMessage.orEmpty())
-            }
-    }
 
     private fun eventSink(): ViewEventSinkFlow<NewPostScreenEvent> = flowOf { event ->
         when (event) {
@@ -85,106 +47,39 @@ internal class NewPostViewModel @Inject constructor(
         }
     }
 
-    private fun onPost(uri: Uri, onPostSuccess: () -> Unit) {
+    private fun onPost(uri: Uri, onPostSuccess: () -> Unit) = viewModelScope.launch {
         uploadImage(uri) {
             onCreatePost(it, onPostSuccess)
         }
     }
 
-    private fun onCreatePost(imageUri: Uri, onPostSuccess: () -> Unit) {
+    private suspend fun onCreatePost(imageUri: Uri, onPostSuccess: () -> Unit) {
         inProgressState.value = true
-        val currentUuid = auth.currentUser?.uid
-        val currentUserName = userState.value?.userName
-        val currentUserImage = userState.value?.imageUrl
-
-        currentUuid?.let {
-            val postUuid = UUID.randomUUID().toString()
-
-            val fillerWords = listOf("the", "be", "to", "is", "of", "and", "or", "a", "in", "it")
-            val searchTerms = descriptionState.value.orEmpty()
-                .split(" ", ".", ",", "?", "!", "#")
-                .map { it.lowercase() }
-                .filter { it.isNotEmpty() && it !in fillerWords }
-
-            val post = PostData(
-                postId = postUuid,
-                userId = currentUuid,
-                username = currentUserName,
-                userImage = currentUserImage,
-                postImage = imageUri.toString(),
-                postDescription = descriptionState.value,
-                time = System.currentTimeMillis(),
-                likes = listOf(),
-                searchTerms = searchTerms
-            )
-            db.collection(POSTS).document(postUuid).set(post)
-                .addOnSuccessListener {
-                    notificationState.value = OneTimeEvent("Post successfully created")
-                    refreshPosts()
-                    onPostSuccess.invoke()
-                }
-                .addOnFailureListener {
-                    errorState.value = it
-                    notificationState.value = OneTimeEvent("Unable to create post")
-                }
-            inProgressState.value = false
-        } ?: run {
-            invalidUserName()
+        createPost.getResult(CreatePost.Params(imageUri.toString(), descriptionState.value))
+            .onSuccess {
+                notificationState.value = OneTimeEvent("Post successfully created")
+                onPostSuccess.invoke()
+            }.onFailure {
+            errorState.value = it
+            notificationState.value = OneTimeEvent("Unable to create post")
         }
     }
 
-    private fun invalidUserName() {
-        Log.e(NewPostViewModel::class.java.name, "Failed to create post currentUuid is null")
-        errorState.value = Throwable("Error: username is unavailable. Unable to create post")
-        onLogout()
-        inProgressState.value = false
-    }
-
-    // TODO extract to interactor and remove duplicate from [ProfileViewModel], interactor should use Firebase repository
-    private fun uploadImage(uri: Uri, onSuccess: (Uri) -> Unit) {
+    private suspend fun uploadImage(uri: Uri, onImageUploaded: suspend (Uri) -> Unit) {
         inProgressState.value = true
-        val storageRef = storage.reference
-        val uuid = UUID.randomUUID()
-        val imageRef = storageRef.child("images/$uuid")
-        val uploadTask = imageRef.putFile(uri)
-
-        uploadTask
-            .addOnSuccessListener {
-                val result = it.metadata?.reference?.downloadUrl
-                result?.addOnSuccessListener {
-                    onSuccess(it)
-                }
+        uploadImage.getResult(uri).onSuccess { uploadedImageUri ->
+            if (uploadedImageUri != null) {
+                onImageUploaded(uploadedImageUri)
+            } else {
+                Log.e(
+                    NewPostViewModel::class.java.name,
+                    "Image upload failed image is $uploadedImageUri"
+                )
+                errorState.value = Throwable("Image upload to firebase failed")
             }
-            .addOnFailureListener {
-                errorState.value = it
-                inProgressState.value = false
-            }
-    }
-
-    // todo create interactor for this
-    private fun onLogout() {
-        auth.signOut()
-        isSignedInState.value = false
-        userState.value = null
-        notificationState.value = OneTimeEvent("Logout")
-    }
-
-    private fun refreshPosts() {
-        val currentUid = auth.currentUser?.uid
-        currentUid?.let {
-            refreshPostsProgressState.value = true
-            db.collection(POSTS)
-                .whereEqualTo("userId", currentUid).get()
-                .addOnSuccessListener { documents ->
-                    postsState.value = convertPosts(documents)
-                }.addOnFailureListener {
-                    errorState.value = it
-                    notificationState.value = OneTimeEvent("Cannot fetch posts")
-                }
-            refreshPostsProgressState.value = false
-        } ?: run {
-            onLogout()
-            errorState.value = Throwable("Error: username unavailable. Unable to refresh posts")
+        }.onFailure {
+            errorState.value = it
+            inProgressState.value = false
         }
     }
 }
