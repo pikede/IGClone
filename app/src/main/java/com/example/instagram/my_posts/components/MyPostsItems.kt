ad3b76daf0250944365310c052523b5719eff555
Prince Ikede <davidikede24@gmail.com>
2025-09-29 22:57:37 -0400

    fixes grid issue by using LazyVerticalGrid

--- app/src/main/java/com/example/instagram/my_posts/components/MyPostsItems.kt
+++ app/src/main/java/com/example/instagram/my_posts/components/MyPostsItems.kt
@@ -2,13 +2,12 @@ package com.example.instagram.my_posts.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -18,28 +17,20 @@ import com.example.instagram.core_ui_components.image.CommonImage
 import com.example.instagram.core_ui_components.video.VideoPlayer
 import com.example.instagram.models.PostData
 
+// todo move to core UI components
 @Composable
 internal fun MyPostsItems(
-    // todo move to core UI components
     posts: List<PostData>,
     onPostClick: (PostData) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(modifier = modifier) {
-        val rows = posts.chunked(3)
-        items(items = rows) { postItems ->
-            Row(
-                modifier = modifier
+    LazyVerticalGrid(GridCells.Fixed(3), modifier = modifier) {
+        items(posts.size) { index ->
+            PostRow(
+                posts[index], Modifier
                     .fillMaxWidth()
                     .height(120.dp)
-            ) {
-                postItems.forEach {
-                    PostRow(
-                        it, Modifier
-                            .weight(1f)
-                            .clickable { onPostClick(it) })
-                }
-            }
+                    .clickable { onPostClick(posts[index]) })
         }
     }
 }
@@ -56,12 +47,8 @@ private fun PostRow(
             modifier = modifier
         )
 
-        else -> {
-            PostImage(
-                imageUrl = postItem.postImage,
-                modifier = modifier
-            )
-        }
+        else -> PostImage(imageUrl = postItem.postImage, modifier = modifier)
+
     }
 }
 
@@ -87,6 +74,7 @@ private fun PostImage(imageUrl: String?, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun MyPostsItemsPreview() {
+    // todo pull this image and video innto app locally for preview
     val samplePosts = listOf(
         PostData(postImage = "https://example.com/image1.jpg"),
         PostData(postVideo = "https://example.com/video1.mp4"),
